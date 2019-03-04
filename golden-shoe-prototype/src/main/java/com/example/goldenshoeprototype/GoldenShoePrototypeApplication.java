3c71e6ac1052a1ca35f5b54f26ee0bca629f0710
Filip Obel <fo33@student.le.ac.uk>
2019-03-04 00:08:28 +0000

    added prototype chat function

---
+++ golden-shoe-prototype/src/main/java/com/example/goldenshoeprototype/GoldenShoePrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.goldenshoeprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenShoePrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenShoePrototypeApplication.class, args);
+	}
+
+}
